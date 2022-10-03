f8f7de3184b6a59656ea0e0e1852e55b94e6de6e
Amulya275 <112035333+Amulya275@users.noreply.github.com>
2022-10-02 22:58:02 -0400

    Update my_AdaBoost.py

--- assignments/assignment5/my_AdaBoost.py
+++ assignments/assignment5/my_AdaBoost.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 from copy import deepcopy
+from pdb import set_trace
 
 class my_AdaBoost:
 
-    def __init__(self, base_estimator = None, n_estimators = 50):
+    def __init__(self, base_estimator = None, n_estimators = 50):        
         # Multi-class Adaboost algorithm (SAMME)
         # base_estimator: the base classifier class, e.g. my_DT
         # n_estimators: # of base_estimator rounds
@@ -17,24 +18,84 @@ class my_AdaBoost:
         # y: list, np.array or pd.Series, dependent variables, int or str
 
         self.classes_ = list(set(list(y)))
+        
         k = len(self.classes_)
-        # write your code below
+        n = len(y)
+        w = np.array([1.0 / n] * n)
+        labels = np.array(y)
+        self.alpha = []
+        for i in range(self.n_estimators):
+            # Sample with replacement from X, with probability w
+            sample = np.random.choice(n, n, p=w)
+            # Train base classifier with sampled training data
+            sampled = X.iloc[sample]
+            sampled.index = range(len(sample))
+            self.estimators[i].fit(sampled, labels[sample])
+            predictions = self.estimators[i].predict(X)
+            diffs = np.array(predictions) != y
+            
+            # Compute error rate and alpha for estimator i
+            error = np.sum(diffs * w)
+            
+            while error >= (1 - 1.0 / k):
+                w = np.array([1.0 / n] * n)
+                sample = np.random.choice(n, n, p=w)
+                # Train base classifier with sampled training data
+                sampled = X.iloc[sample]
+                sampled.index = range(len(sample))
+                self.estimators[i].fit(sampled, labels[sample])
+                predictions = self.estimators[i].predict(X)
+                diffs = np.array(predictions) != y
+                print(diffs)
+                # Compute error rate and alpha for estimator i
+                 
+                error = np.sum(diffs * w)
+                print(error)
+            # If one base estimator predicts perfectly,
+            # Use that base estimator only
+            if error == 0:
+                self.alpha = [1]
+                self.estimators = [self.estimators[i]]
+                break
+            # Compute alpha for estimator i (don't forget to use k for multi-class)
+            
+            
+            self.alpha.append( (np.log(1-error)/error) + (np.log(k-1) ) )
+
+            # Update wi
+            for i in range(len(w)):
+                if (diffs[i]==True):
+                    w[i]=w[i]*np.exp( self.alpha[-1])  
+                else:
+                    w[i]= w[i]
+            w=w/np.sum(w)
+        
+        # Normalize alpha
+        self.alpha = self.alpha / np.sum(self.alpha)
+    
         return
 
     def predict(self, X):
         # X: pd.DataFrame, independent variables, float
         # return predictions: list
-        # write your code below
+        probs = self.predict_proba(X)
+        predictions = [self.classes_[np.argmax(prob)] for prob in probs.to_numpy()]
         return predictions
 
-    def predict_proba(self, X):
+    def predict_proba(self, X): 
         # X: pd.DataFrame, independent variables, float
         # prob: what percentage of the base estimators predict input as class C
         # prob(x)[C] = sum(alpha[j] * (base_model[j].predict(x) == C))
         # return probs = pd.DataFrame(list of prob, columns = self.classes_)
+        # Note that len(self.estimators) can sometimes be different from self.n_estimators
         # write your code below
-
-
+        probs = {}
+        for label in self.classes_:
+            probs[label] =  np.zeros(len(X))
+            for i in range(len(self.estimators)):
+                probs[label]+=(self.alpha[i] * ((self.estimators[i].predict(X)) == label))
+        
+        probs = pd.DataFrame(probs, columns=self.classes_)
         return probs
 
 
